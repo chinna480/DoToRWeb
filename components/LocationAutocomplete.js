e0fcdaede0757b76320e3d27089004c61d2a2270
chinna480 <chinna480@gmail.com>
2026-05-23 15:17:33 +0530

    fix: fix trailing newline in LocationAutocomplete.js, add .gitignore for dotorweb

--- components/LocationAutocomplete.js
+++ components/LocationAutocomplete.js
@@ -119,7 +119,7 @@ export default function LocationAutocomplete({
                   </Text>
                 </View>
               </TouchableOpacity>
-            }
+            )}
           />
         </View>
       )}
